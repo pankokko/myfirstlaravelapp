1a91dd3f5b5965e22f158702efa35d2ec1932cc8
pankokko <teshi41@gmail.com>
2019-12-16 18:31:34 +0900

    Initial commit

---
+++ resources/views/items/index.blade.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <link rel="stylesheet" href="/css/app.css"> 
+
+<title>Home/index</title>
+</head>
+<body>
+<header class="mainpage-header">
+  <h1 class="mainpage-header-logo">
+    <a href="/"></a>
+    PHOTOWEB
+    <span class="mainpage-header-logo__text">お気に入りの写真を投稿しよう！</span>
+  </h1>
+   <nav class="mainpage-header__nav">
+     <ul class="mainpage-header__nav-ul">
+      <li class="mainpage-header__nav-ul-list">
+        <a class="mainpage-header__nav-ul-list-link" href="#">
+          <i class="fas fa-camera"></i> 
+          写真を見る
+        </a>
+     </li>  
+     </ul>   
+    </nav> 
+</header>
+
+
+</body>
+</html>
