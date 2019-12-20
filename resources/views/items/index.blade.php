d657adef7cc320692e658b1e4a976d94d1793ed3
pankokko <teshi41@gmail.com>
2019-12-20 11:50:25 +0800

    indexページのnavバー作成中

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.default')
-
 @section('content')
   <div class="pictures-wrapper">
     <div class="picture-wrapper_photo">
@@ -8,5 +7,14 @@
       </a>
     </div> 
   </div>
-
+  <div class="top-nav"> 
+    <div class="top-nav-wrapper">
+      <div class="top-nav-wrapper-list">
+        <a class="top-nav-wrapper-list-link" href="#">おまかせ</a> 
+      </div>
+      <div class="top-nav-wrapper-list">
+        <a class="top-nav-wrapper-list-link" href="#">新着</a> 
+      </div>
+    <div>
+  </div>
 @endsection
