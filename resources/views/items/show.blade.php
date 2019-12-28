ee83f282053c213ba0daddb96aeeef60feab42dc
pankokko <teshi41@gmail.com>
2019-12-28 15:30:32 +0900

    modal.scssを追加

--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -1,5 +1,4 @@
 @extends("layouts.default")
-
 @section("content")
   <div class="show-wrapper">
     <a href="#modal-01">
@@ -14,5 +13,25 @@
           <a href="#!" class="modal-close">×</a>
       </div>
     </div>
+      <div class=other-pics-wrapper>
+        <p class="user-other-pics">ユーザーさんの他の作品</p>
+        <div class="show-pictures">
+          <div class="show-pictures-wrapper"> 
+            <a  class="show-pictures-wrapper-link" href="#">
+              <img class="show-pictures-wrapper-picture" src="{{asset("images/DSC00178.JPG")}}"width="100%" height="180px">
+            </a>
+          </div>
+          <div class="show-pictures-wrapper"> 
+            <a  class="show-pictures-wrapper-link" href="#">
+              <img class="show-pictures-wrapper-picture" src="{{asset("images/DSC00178.JPG")}}"width="100%" height="180px">
+            </a>
+          </div>
+          <div class="show-pictures-wrapper"> 
+            <a  class="show-pictures-wrapper-link" href="#">
+              <img class="show-pictures-wrapper-picture" src="{{asset("images/DSC00178.JPG")}}"width="100%" height="180px">
+            </a>
+          </div>
+        </div>
+      </div>
   </div>
 @endsection 
