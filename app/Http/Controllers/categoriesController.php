20c4911aa48f3ca82a3a3c17b56aeecf4a9f1f1a
pankokko <teshi41@gmail.com>
2020-01-08 14:47:42 +0900

    セーブ

--- app/Http/Controllers/categoriesController.php
+++ app/Http/Controllers/categoriesController.php
@@ -10,7 +10,16 @@ class CategoriesController extends Controller
 
     public function index(request $request)
     {
-        return view("categories/index");
+       $landscape = Category::find(1)->items->count();
+       $city = Category::find(2)->items->count();
+       $person= Category::find(3)->items->count();
+       $creature= Category::find(4)->items->count();
+
+    //  eval(\Psy\sh());
+    return view("categories/index",compact("landscape","city","person","creature"));
     }
+       
+    
    
+
 }
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -14,28 +14,28 @@
       <a class="categories-wrapper-container-picture-link" href="#">
         <img class="categories-wrapper-container-picture-link-img" src="{{asset('/storage/thumbnail/1578143856.DSC00233.JPG')}}"width="260" height="300">
       </a>
-      <h1 class="categories-wrapper-container-picture-category">風景 <span class="picture-count">(200枚)</span></h1>
+    <h1 class="categories-wrapper-container-picture-category">風景 <span class="picture-count">({{$landscape}}枚)</span></h1>
       <p class="categories-wrapper-container-picture-text">山や海等自然の写真をピックアップしています。<br>自然の美しい写真をお楽しみください</p>
     </div>
     <div class="categories-wrapper-container-picture"> 
       <a class="categories-wrapper-container-picture-link" href="#">
         <img class="categories-wrapper-container-picture-link-img" src="{{asset('/storage/thumbnail/1578143856.DSC00233.JPG')}}"width="260" height="300">
       </a>
-      <h1 class="categories-wrapper-container-picture-category">風景 <span class="picture-count">(200枚)</span></h1>
+    <h1 class="categories-wrapper-container-picture-category">風景 <span class="picture-count">({{$city}}枚)</span></h1>
       <p class="categories-wrapper-container-picture-text">山や海等自然の写真をピックアップしています。<br>自然の美しい写真をお楽しみください</p>
     </div>
     <div class="categories-wrapper-container-picture"> 
       <a class="categories-wrapper-container-picture-link" href="#">
         <img class="categories-wrapper-container-picture-link-img" src="{{asset('/storage/thumbnail/1578143856.DSC00233.JPG')}}"width="260" height="300">
       </a>
-      <h1 class="categories-wrapper-container-picture-category">風景 <span class="picture-count">(200枚)</span></h1>
+    <h1 class="categories-wrapper-container-picture-category">風景 <span class="picture-count">({{$person}}枚)</span></h1>
       <p class="categories-wrapper-container-picture-text">山や海等自然の写真をピックアップしています。<br>自然の美しい写真をお楽しみください</p>
     </div>
     <div class="categories-wrapper-container-picture"> 
       <a class="categories-wrapper-container-picture-link" href="#">
         <img class="categories-wrapper-container-picture-link-img" src="{{asset('/storage/thumbnail/1578143856.DSC00233.JPG')}}"width="260" height="300">
       </a>
-      <h1 class="categories-wrapper-container-picture-category">風景 <span class="picture-count">(200枚)</span></h1>
+      <h1 class="categories-wrapper-container-picture-category">風景 <span class="picture-count">({{$creature}}枚)</span></h1>
       <p class="categories-wrapper-container-picture-text">山や海等自然の写真をピックアップしています。<br>自然の美しい写真をお楽しみください</p>
     </div>
   </div>
